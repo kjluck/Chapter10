d8f5dbf108de31fbf9ad66f9294918114195238c
kjluck <99297901+kjluck@users.noreply.github.com>
2022-04-27 12:37:09 -0400

    Chapter10Apply

---
+++ chapter10/apply/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+Student Name: Kenneth Luck
+Date: Wed., April 27, 2022
+File: script.js
+*/
+
+//Function to display content
+function content () {
+  var text = document.getElementById ("new");
+  text.textContent = "Hi. I am a new paragraph that was created with
+  a JavaScript function.";
+  text.style.color = "#c0145f";
+  text.style.fontsize = "2e,";
+}

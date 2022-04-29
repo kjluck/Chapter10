4aa62c82a057e6c1d7c5d9a90240cd09d10526ef
kjluck <99297901+kjluck@users.noreply.github.com>
2022-04-29 19:18:28 -0400

    Chapter10update

--- chapter10/analyze/scripts/script.js
+++ chapter10/analyze/scripts/script.js
@@ -6,8 +6,27 @@
 
 //Function to display the first picture
 function pic1 {
-    imgSource.src = "images/trunk-bay.jpg"
+    var figElement = document.getElementById("placeholder");
+    var imgSource = document.getElementById("image");
+    var figCap = document.querySelector("figcaption");
+    imgSource.src = "images/trunk-bay.jpg";
     imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block"
+    figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
-} 
+}
+
+//Function to display the second picture
+function pic2 {
+  imgSource.src = "images/sanjuan.jpg";
+  imgSource.alt = "Elevated view of San Jaun coast";
+  figElement.style.display = "block";
+  figCap.textContent = "Coast of San Jaun";
+}
+
+//Function to display third pciture
+function pic3 {
+  imgSource.src = "images/curacao.jpg";
+  imgSource.alt = "The blue waters of Curacao";
+  figElement.style.display = "block";
+  figCap.textContent = "Curacao";
+}

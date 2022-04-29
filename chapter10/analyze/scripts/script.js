08237cd9ae67d08fb46f974ea08dddea528c19e6
kjluck <99297901+kjluck@users.noreply.github.com>
2022-04-29 15:12:12 -0400

    Chapter10Analyze

--- chapter10/analyze/scripts/script.js
+++ chapter10/analyze/scripts/script.js
@@ -1,12 +1,13 @@
 /*
-    Student Name:
+    Student Name: Kenneth Luck
     File Name: script.js
-    Date: 
+    Date: Aptil 28, 2022
 */
 
 //Function to display the first picture
 function pic1 {
     imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block"
     figCap.textContent = "Trunk Bay in St. John";
+} 
--- chapter10/extend/scripts/script.js
+++ chapter10/extend/scripts/script.js
@@ -1,5 +1,14 @@
 /*
     Student Name:
     File Name: script.js
-    Date: 
+    Date:
 */
+
+
+//Function to display content
+function content () {
+  var text = document.getElementById ("new");
+  text.textContent = "Hi. I am a new paragraph that was created witha JavaScript function.";
+  text.style.color = "#c0145f";
+  text.style.fontsize = "2e,";
+}
